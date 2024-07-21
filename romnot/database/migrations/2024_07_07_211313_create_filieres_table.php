1e6344e406f8d4693d4c2a4cfbb75dcb1f634e95
Christy-Banga <61908366+Christy-Banga@users.noreply.github.com>
2024-07-21 18:54:07 +0000

    essai

--- romnot/database/migrations/2024_07_07_211313_create_filieres_table.php
+++ romnot/database/migrations/2024_07_07_211313_create_filieres_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('filieres', function (Blueprint $table) {
             $table->id();
+            $table->string('nomfiliere');
             $table->timestamps();
         });
     }

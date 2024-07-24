47549b88114ca540bd9e8f8ddeaaedbbda4f5577
Christy-Banga <61908366+Christy-Banga@users.noreply.github.com>
2024-07-24 18:37:08 +0000

    Listedemandeinscription

---
+++ romnot/app/Http/Controllers/DemandeInscriptionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DemandeInscription;
+use App\Http\Requests\StoreDemandeInscriptionRequest;
+use App\Http\Requests\UpdateDemandeInscriptionRequest;
+
+class DemandeInscriptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDemandeInscriptionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DemandeInscription $demandeInscription)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DemandeInscription $demandeInscription)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDemandeInscriptionRequest $request, DemandeInscription $demandeInscription)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DemandeInscription $demandeInscription)
+    {
+        //
+    }
+}
